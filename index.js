7576e8ea951091af9c32ad0f6cbac4c64af7881c
Paragon Initiative Enterprises <security@paragonie.com>
2019-10-09 03:40:37 -0400

    Throw a SodiumError instead of a base Error

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
     CryptographyKey: require('./lib/cryptography-key'),
     Ed25519PublicKey: require('./lib/keytypes/ed25519pk'),
     Ed25519SecretKey: require('./lib/keytypes/ed25519sk'),
+    SodiumError: require('./lib/sodium-error'),
     SodiumPlus: require('./lib/sodiumplus'),
     SodiumPolyfill: require('./lib/polyfill'),
     X25519PublicKey: require('./lib/keytypes/x25519pk'),
