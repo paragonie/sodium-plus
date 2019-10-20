1f6049696c48186051afa72bfc03e166d641edff
Paragon Initiative Enterprises <security@paragonie.com>
2019-10-19 23:16:36 -0400

    Pass to subprocess correctly

--- build.sh
+++ build.sh
@@ -3,11 +3,13 @@
 basedir=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 path="${basedir}/build/remove-sodium-native.sh"
 
-ret=$(bash "${path}")
+bash "${path}"
+ret=$?
 if [[ $ret -ne 0 ]]; then
   echo "Exiting..."
   exit "${ret}"
 fi
+echo "Return ${ret}"
 
 browserify index.js > dist/sodium-plus.js
 browserify index.js -p tinyify > dist/sodium-plus.min.js
